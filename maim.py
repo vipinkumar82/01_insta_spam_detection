a281adc2aee5c09b6a76bd371585c593feac8b23
Vipin kumar <vipink8272@gmail.com>
2026-03-13 16:20:30 +0530

    Update main.py

--- maim.py
+++ maim.py
@@ -1,9 +1,10 @@
+# main.py
 import sys
 from PyQt5.QtWidgets import QApplication
 from gui import SpamDetector
 
-app = QApplication(sys.argv)
-window = SpamDetector()
-window.show()
-
-sys.exit(app.exec_())
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = SpamDetector()
+    window.show()
+    sys.exit(app.exec_())
